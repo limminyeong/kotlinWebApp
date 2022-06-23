3d64ee82078b55c2535133f72035dfb6575e3083
mylim <lim.minyeong.hcg@gmail.com>
2022-06-23 13:24:53 +0900

    추가 설정

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.happybirthday
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
@@ -15,8 +17,19 @@ class MainActivity : AppCompatActivity() {
             javaScriptEnabled = true
             domStorageEnabled = true
             setSupportMultipleWindows(true)
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportZoom(true)
+            useWideViewPort = true
+            displayZoomControls = true
+            allowFileAccess = true
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ) {
+                mediaPlaybackRequiresUserGesture = false
+            }
         }
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://performance.theplus.io/")
+        webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            loadUrl("https://performance.theplus.io/")
+        }
     }
 }
